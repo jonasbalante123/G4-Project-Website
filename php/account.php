ded12d83a0f05ad31187d1463940ed0ebaf9c707
jonasbalante123 <43815203+jonasbalante123@users.noreply.github.com>
2023-06-01 14:57:11 +0800

    a
    weee

--- php/account.php
+++ php/account.php
@@ -1,14 +1,42 @@
-<?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve form data
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
+    // Create a database connection
+    $servername = "localhost";
+    $username_db = "root";
+    $password_db = "";
+    $dbname = "quiz master";
 
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+    $conn = new mysqli($servername, $username_db, $password_db, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare and execute the SQL query
+    $stmt = $conn->prepare("INSERT INTO accounts (Username, Password, Gmail) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $password, $email);
+    $stmt->execute();
+
+    // Check if the query was successful
+    if ($stmt->affected_rows > 0) {
+        echo '<script>';
+        echo 'alert("Account created successfully!");';
+        echo 'window.opener.location.reload();'; // Reload the parent window
+        echo 'window.close();'; // Close the current window
+        echo '</script>';
+    } else {
+        echo '<script>';
+        echo 'alert("Error occurred while creating the account.");';
+        echo 'window.location.href = "../signup.html";';
+        echo '</script>';
+    }
+
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
 }
- "Connected successfully";
 ?>
